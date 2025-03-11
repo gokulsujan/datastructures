ec007cba8b5a1bec16d63bf97c8dd7bbad903249
Gokul Sujan <gokulsujan.dev@gmail.com>
2025-03-11 21:03:05 +0530

    Implemented preorder traversal in tree

--- tree/binary_tree/BinaryTree.java
+++ tree/binary_tree/BinaryTree.java
@@ -2,6 +2,7 @@ public class BinaryTree {
     Node root;
 
     static int buildTreeIdx = -1;
+
     public Node buildTreePreOrder(int nodes[]) {
         buildTreeIdx++;
         if (nodes[buildTreeIdx] == -1) {
@@ -12,6 +13,16 @@ public class BinaryTree {
         newNode.left = buildTreePreOrder(nodes);
         newNode.right = buildTreePreOrder(nodes);
         return newNode;
+    }
+
+    public void preorderTraversal(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        System.out.print(root.data + " ");
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
     }
 }
 
--- tree/binary_tree/Main.java
+++ tree/binary_tree/Main.java
@@ -1,12 +1,14 @@
-public class BuildPreOrderTree {
+public class Main {
     public static void main(String[] args) {
         int tree[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
 
+        // Build tree in preorder
         BinaryTree binaryTree = new BinaryTree();
         binaryTree.root = binaryTree.buildTreePreOrder(tree);
 
-        System.out.println(binaryTree.root.data);
-        System.out.println(binaryTree.root.right.right.data);
-
+        // Preorder traversal
+        System.out.print("Preorder traversal: ");
+        binaryTree.preorderTraversal(binaryTree.root);
+        System.out.println("");
     }
 }
